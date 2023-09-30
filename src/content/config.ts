c27acf98a30462bb1da280bb2eccaf047e8d5620
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-09-30 12:44:18 +0200

    created blog list pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
+const langSchema = z.enum(['en_US', 'hu_HU']);
+export type Lang = z.infer<typeof langSchema>;
+
 const postsCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
 		date: z.date(),
 		description: z.string(),
-		lang: z.enum(['en_US', 'hu_HU']),
+		lang: langSchema,
 		title: z.string(),
 		tags: z.string(),
 	}),
