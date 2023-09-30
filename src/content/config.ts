035f119304513ddbef9c07c5e66ed191d6897d8b
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-09-30 12:37:57 +0200

    started creating layouts and components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,11 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 const postsCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
 		date: z.date(),
 		description: z.string(),
+		lang: z.enum(['en_US', 'hu_HU']),
 		title: z.string(),
 		tags: z.string(),
 	}),
