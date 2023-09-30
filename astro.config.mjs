a061a25636c537e35443aa11657a480d1044bfa8
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2023-09-30 15:42:04 +0200

    added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import robots from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [robots(), sitemap(), tailwind()],
+	site: 'https://juzraai.github.io',
 });
